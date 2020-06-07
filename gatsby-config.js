b0a761020c4effc92ba5cf63b5683cbf8697a968
Jeyvison Nascimento <jeyvison.nascimento@liferay.com>
2020-06-07 10:23:37 -0300

    Initial commit from gatsby: (https://github.com/gatsbyjs/gatsby-starter-blog-theme.git)

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `J's Blog`,
+    author: `Jeyvison Nascimento`,
+    description: `My site description...`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/jeyvisonn`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/jeyvison`,
+      },
+    ],
+  },
+}
